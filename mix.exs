e7378bd5e27156609ba7e8ff88f85051be542727
skovsgaard <jonas.skovsgaard.christensen@gmail.com>
2016-06-08 00:23:15 +0200

    Cautiously bump minor versions in honor of no one

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule BusyBoard.Mixfile do
 
   def project do
     [app: :busy_board,
-     version: "0.0.1",
+     version: "0.1.0",
      elixir: "~> 1.2",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
