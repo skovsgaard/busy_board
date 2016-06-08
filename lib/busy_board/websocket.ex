99fd51fc97c412f951bdc8582be03e8a966304be
skovsgaard <jonas.skovsgaard.christensen@gmail.com>
2016-06-08 22:59:40 +0200

    Wire up the first websocket handler

--- lib/busy_board/websocket.ex
+++ lib/busy_board/websocket.ex
@@ -1,11 +1,20 @@
 defmodule BusyBoard.Websocket do
+  alias BusyBoard.Server
+
   @behaviour :cowboy_websocket_handler
+
+  # OTP behaviour callbacks
 
   def init(_, _req, _opts), do: {:upgrade, :protocol, :cowboy_websocket}
 
   def websocket_init(_type, req, _opts) do
     state = %{}
     {:ok, req, state}
+  end
+
+  def websocket_handle({:text, "all"}, req, state) do
+    {:ok, all_people} = Server.all |> Poison.encode
+    {:reply, {:text, all_people}, req, state}
   end
 
   def websocket_handle({:text, "ping"}, req, state),
