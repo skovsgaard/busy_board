6036bb2e5013a073260bc800849ec25125be4682
skovsgaard <jonas.skovsgaard.christensen@gmail.com>
2016-06-14 22:53:57 +0200

    Add reconnect loop on websocket client

--- priv/static/js/socketeer.js
+++ priv/static/js/socketeer.js
@@ -1,18 +1,36 @@
 const socketeer = function(url) {
-  var ws = new WebSocket(url)
+  (function start() {
+    var ws = new WebSocket(url)
 
-  ws.onopen = (openEvent) => {
-    ws.send("all")
-    util.setRegisterClick(ws)
-  }
+    ws.onopen = (openEvent) => {
+      console.log("Successfully connected to BusyBoard server")
+      ws.send("all")
+      util.setRegisterClick(ws)
+      util.removeByClass(".err-box")
+    }
 
-  ws.onmessage = (msgEvent) => {
-    console.log(msgEvent.data)
-    util
-      .renderPeople(JSON.parse(msgEvent.data))
-      .forEach((person) => util.setPersonClick(person, ws))
-  }
+    ws.onmessage = (msgEvent) => {
+      console.log(msgEvent.data)
+      util
+        .renderPeople(JSON.parse(msgEvent.data))
+        .forEach((person) => util.setPersonClick(person, ws))
+    }
 
-  ws.onerror = (errEvent) => console.log(err)
+    ws.onerror = (errEvent) => {
+      console.log(errEvent)
+    }
+
+    ws.onclose = (closeEvent) => {
+      util.removeByClass(".err-box")
+      util.append({
+        query: "header",
+        tag: "section",
+        class: "twelve columns err-box",
+        content: util.errMsg("Connection to the BusyBoard server was lost...")
+      })
+
+      setTimeout(() => start(), 5000)
+    }
+  })()
 }
 
