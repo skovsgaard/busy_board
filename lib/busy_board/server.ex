574080cd8ec916b7de28f6ab4a58e766e80b8182
skovsgaard <jonas.skovsgaard.christensen@gmail.com>
2016-06-14 20:55:21 +0200

    Add persistence to data store

--- lib/busy_board/server.ex
+++ lib/busy_board/server.ex
@@ -6,10 +6,11 @@ defmodule BusyBoard.Server do
   def start_link do
     table = :people
     {:ok, pid} = GenServer.start_link(@mod, table, name: @mod)
-
-    table
-    |> :ets.new([:named_table, :protected])
-    |> :ets.give_away(pid, :ok)
+    {:ok, dets_label} = :dets.open_file(table, [file: "busy_board.dat"])
+    :ets.new(table, [:named_table, :protected])
+    :dets.to_ets(dets_label, table)
+    :ets.give_away(table, pid, :ok)
+    :dets.close(dets_label)
 
     {:ok, pid}
   end
@@ -20,6 +21,8 @@ defmodule BusyBoard.Server do
 
   def put(person), do: GenServer.call(@mod, {:put, person})
 
+  def persist, do: GenServer.call(@mod, :persist)
+
   # OTP callbacks
 
   def handle_call(:all, _from, table) do
@@ -28,11 +31,13 @@ defmodule BusyBoard.Server do
 
   def handle_call({:put, {name, status}}, _from, table) do
     :ets.insert table, {name, status}
+    persist table
     {:reply, table |> all_people |> Poison.encode!, table}
   end
 
   def handle_call({:put, person}, _from, table) do
     :ets.insert table, {person["name"], :unavailable}
+    persist table
     {:reply, :ok, table}
   end
 
@@ -42,5 +47,11 @@ defmodule BusyBoard.Server do
     :ets.foldr fn {name, status}, acc ->
       acc ++ [%{name: name, status: status}]
     end, [], table
+  end
+
+  defp persist(ets_label) do
+    {:ok, dets_label} = :dets.open_file(ets_label, [file: "busy_board.dat"])
+    :ets.to_dets(ets_label, dets_label)
+    :ok = :dets.close(dets_label)
   end
 end
